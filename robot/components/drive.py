a100215050afdf56a586d2f31fae11f553f19447
ErikBoesen <me@erikboesen.com>
2017-12-30 20:48:08 -0500

    Add missing docstrings to drive.py

--- robot/components/drive.py
+++ robot/components/drive.py
@@ -3,17 +3,28 @@ import wpilib.drive
 
 
 class Drive:
+    """
+    Handle robot drivetrain.
+
+    All drive interaction must go through this class.
+    """
     robot_drive: wpilib.drive.DifferentialDrive
 
     def __init__(self):
         self.enabled = False
 
     def on_enable(self):
+        """
+        Tasks for initialization upon injection.
+        """
         pass
 
     def move(self, y, rotation):
         """
         Move robot.
+
+        :param y: y-axis movement speed.
+        :param rotation: Rotation speed.
         """
         pass
 
