aead5c32ffca9f197f3483e49bc321cb8ff135af
JosephCarpenter <carp5@rcn.com>
2018-01-17 17:58:27 -0500

    Add state to curve into switch

--- robot/autonomous/modular.py
+++ robot/autonomous/modular.py
@@ -31,30 +31,52 @@ class Modular(AutonomousStateMachine):
         """
         Decide how to begin the autonomous.
         """
+        # TODO: Decide which method to use to reach switch:
+        # gradual turn or move straight forward and turn 90 degrees
+        # As of now, we are using gradual turning
         self.next_state('advance_initial')
 
-    @timed_state(duration=1, next_state='advance_rotate')
+    # Duration needs to be short so that the robot does not crash into the cube stack if it is in the middle
+    @timed_state(duration=0.3, next_state='advance_rotate')
     def advance_initial(self):
         """
-        Cross autonomous baseline.
+        Give the robot some distance from the starting point.
         """
-        self.drive.move(1, 0)
+        self.drive.move(0.6, 0)
 
-    @timed_state(duration=0.3)
+    @timed_state(duration=0.5)
     def advance_rotate(self):
         """
-        Rotate robot, either to avoid central cube pile or to get to the sides of the switch.
+        Rotate robot to face the outside of the arena, so it can curve back around.
         """
         # TODO: DRY
+
+        # Numbers are approximate
         if self.position == 'left':
             self.drive.move(0.2, -0.5)
         elif self.position == 'right':
             self.drive.move(0.2, 0.5)
-        elif self.position == 'middle':
+        else:
             if self.plates[0] == 'L':
                 self.drive.move(0.2, -0.5)
             elif self.plates[0] == 'R':
                 self.drive.move(0.2, 0.5)
+
+    @timed_state(duration=3)
+    def advance_rotate_final(self):
+        """
+        Curve robot around to switch.
+        """
+        # TODO: DRY
+        if self.position == 'left':
+            self.drive.move(0.5, 0.5)
+        elif self.position == 'right':
+            self.drive.move(0.5, -0.5)
+        else:
+            if self.plates[0] == 'L':
+                self.drive.move(0.7, 0.5)
+            elif self.plates[0] == 'R':
+                self.drive.move(0.7, -0.5)
 
         if self.drop:
             self.next_state('drop')
