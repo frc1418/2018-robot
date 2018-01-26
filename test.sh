8df4260dfdf4824b0f8199e235f0570890cb6503
ErikBoesen <me@erikboesen.com>
2018-01-25 21:48:43 -0500

    Ignore E702 in tests (PyCQA/pycodestyle#682)

--- test.sh
+++ test.sh
@@ -2,4 +2,7 @@
 set -e
 
 python3 robot/robot.py test
-pycodestyle . --ignore=E501
+# E501 line too long
+# E701 multiple statements on one line
+# Currently, E701 takes issue with some type annotations. (PyCQA/pycodestyle#682)
+pycodestyle . --ignore=E501,E701
