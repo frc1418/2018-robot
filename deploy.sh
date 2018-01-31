117bfc192fbe3ae1025e2ec57d3e2be59a09e3c7
ErikBoesen <me@erikboesen.com>
2018-01-31 17:36:28 -0500

    Only switch to robot network if tests pass

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@
 start_network=$(networksetup -getairportnetwork en0 | cut -d ' ' -f 4)
 robot_network=1418
 
-networksetup -setairportnetwork en0 $robot_network
-python3 robot/robot.py deploy
+python3 robot/robot.py test && networksetup -setairportnetwork en0 $robot_network || exit 1
+python3 robot/robot.py deploy --skip-tests
 networksetup -setairportnetwork en0 $start_network
