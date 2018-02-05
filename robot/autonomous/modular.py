421a48ecfb32e88cdcc11591f95669a1f59574f1
ErikBoesen <me@erikboesen.com>
2018-02-05 17:46:31 -0500

    Modular: complete tested side switch

--- robot/autonomous/modular.py
+++ robot/autonomous/modular.py
@@ -11,12 +11,12 @@ class Modular(AutonomousStateMachine):
     drive = drive.Drive
     crane = crane.Crane
 
-    position = ntproperty('/autonomous/position', '')
-    plates = ntproperty('/robot/plates', '')
+    position = ntproperty('/autonomous/position', 'left')
+    plates = ntproperty('/robot/plates', 'LRL')
 
     advance = tunable(True)
     switch = tunable(True)
-    scale = tunable(True)
+    scale = tunable(False)
 
     @state(first=True)
     def start(self):
@@ -40,186 +40,42 @@ class Modular(AutonomousStateMachine):
         self.drive.move(0.8, 0)
 
     # Duration needs to be short so that the robot does not crash into the cube stack if it is in the middle
-    @timed_state(duration=0.3, next_state='switch_rotate')
+    @timed_state(duration=1.7, next_state='switch_rotate_side')
     def switch_initial(self):
         """
         Give the robot some distance from the starting point.
         """
-        self.drive.move(0.6, 0)
+        self.crane.move(0.4)
+        self.drive.move(0.7, 0)
 
-    @timed_state(duration=0.5, next_state='switch_curve')
-    def switch_rotate(self):
+    @timed_state(duration=0.6, next_state='switch_drop_side')
+    def switch_rotate_side(self):
         """
-        Rotate robot to face the outside of the arena, so it can curve back around.
+        Rotate robot to face the switch.
         """
-        # TODO: DRY
-
+        self.crane.move(0.4)
         # Numbers are approximate
         if self.position == 'left':
-            if self.plates[0] == 'L':
-                self.drive.move(0.2, -0.5)
-            else:
-                self.drive.move(0.2, 0.8)
+            self.drive.move(0.2, 1)
         elif self.position == 'right':
-            if self.plates[0] == 'R':
-                self.drive.move(0.2, 0.5)
-            else:
-                self.drive.move(0.2, -0.8)
+            self.drive.move(0.2, -1)
         else:
             if self.plates[0] == 'L':
-                self.drive.move(0.2, -0.5)
+                self.drive.move(0.2, -1)
             elif self.plates[0] == 'R':
-                self.drive.move(0.2, 0.5)
+                self.drive.move(0.2, 1)
         # The robot will now have the opportunity to pursue different paths
         # It can either try to score on the switch, the switch and the scale,
         # just the scale, or neither, depending on what is selected in the UI
         if self.switch:
-            self.next_state('switch_curve')
-        # TODO: If only scale is selected and not switch,
-        # make a separate state path to go directly to scale
-        # elif self.scale:
-        #    self.next_state('lorem_ipsum')
-
-    @timed_state(duration=3)
-    def switch_curve(self):
-        """
-        Curve robot around to switch.
-        """
-        # TODO: DRY
-        if self.position == 'left':
-            if self.plates[0] == 'L':
-                self.drive.move(0.5, 0.5)
-            else:
-                self.drive.move(0.7, -0.3)
-        elif self.position == 'right':
-            if self.plates[0] == 'R':
-                self.drive.move(0.5, -0.5)
-            else:
-                self.drive.move(0.7, 0.3)
-        else:
-            # Since the middle position has now reached the same point as either the left or right position,
-            # the middle postion is reassigned to either the left or right, depending on its plate
-            if self.plates[0] == 'L':
-                self.drive.move(0.6, 0.5)
-                self.position = 'left'
-            elif self.plates[0] == 'R':
-                self.drive.move(0.6, -0.5)
-                self.position = 'right'
-        self.next_state('switch_curve_final')
-
-    @timed_state(duration=1, next_state='drop_switch')
-    def switch_curve_final(self):
-        """
-        Second part to curving maneuver to curve the robot back into the switch.
-        If the robot was already on the correct side of the switch, this second
-        state will not take place because it is not needed.
-        """
-        if self.position == 'left' and self.plates[0] == 'R':
-            self.drive.move(0.4, -0.8)
-            # Position is now switched
-            self.position = 'right'
-        elif self.position == 'right' and self.plates[0] == 'L':
-            self.drive.move(0.4, 0.8)
-            self.position = 'left'
+            # TODO: Make this do things
+            # self.next_state('switch_curve')
+            pass
 
     @timed_state(duration=0.5)
-    def drop_switch(self):
+    def switch_drop_side(self):
         """
         Drop preloaded cube in switch.
         """
-        # TODO: Complete once manipulator configuration known
-        if self.scale:
-            self.next_state('back_up')
-
-    @timed_state(duration=0.3, next_state='pickup_rotate')
-    def back_up(self):
-        """
-        Back up from switch to provide clearance space.
-        """
-        self.drive.move(-1, 0)
-
-    @timed_state(duration=0.5, next_state='pickup_curve')
-    def pickup_rotate(self):
-        """
-        Rotate into position for the curve method.
-        """
-        if self.position == 'left':
-            self.drive.move(0.2, -0.5)
-        if self.position == 'right':
-            self.drive.move(0.2, 0.5)
-
-    @timed_state(duration=1)
-    def pickup_curve(self):
-        """
-        Curve into cube so that it is in position for pickup.
-        """
-        if self.position == 'left':
-            self.drive.move(0.7, 0.5)
-        elif self.position == 'right':
-            self.drive.move(0.7, -0.5)
-
-        self.next_state('pickup_cube')
-
-    @timed_state(duration=1)
-    def pickup_cube(self):
-        """
-        Pick up cube from ground.
-        """
-        # TODO: Complete once manipulator configuration known
-        self.next_state('curve_back')
-
-    @timed_state(duration=1, next_state='rotate_scale')
-    def curve_back(self):
-        """
-        Go back to previous position for easier access to the scale.
-        Or if the robot is on the wrong side of the scale,
-        back up just a little bit
-        """
-        if self.position == 'left':
-            if self.plates[1] == 'L':
-                self.drive.move(-0.7, 0.5)
-            else:
-                self.drive.move(-0.4, 0.5)
-        elif self.position == 'right':
-            if self.plates[1] == 'R':
-                self.drive.move(-0.7, -0.5)
-            else:
-                self.drive.move(-0.4, 0.5)
-
-    @timed_state(duration=0.5, next_state='curve_scale')
-    def rotate_scale(self):
-        """
-        Rotate into position to curve into scale.
-        """
-        if self.position == 'left':
-            if self.plates[1] == 'L':
-                self.drive.move(0.2, 0.5)
-            else:
-                self.drive.move(0.2, 0.3)
-        elif self.position == 'right':
-            if self.plates[1] == 'R':
-                self.drive.move(0.2, -0.5)
-            else:
-                self.drive.move(0.2, -0.3)
-
-    @timed_state(duration=1)
-    def curve_scale(self):
-        """
-        Curve around to the side of the scale, to get the robot into positon for the scale.
-        """
-        if self.position == 'left':
-            if self.plates[1] == 'L':
-                self.drive.move(0.7, -0.5)
-            else:
-                self.drive.move(0.4, 0.4)
-        elif self.position == 'right':
-            if self.plates[1] == 'R':
-                self.drive.move(0.7, -0.5)
-            else:
-                self.drive.move(0.4, -0.4)
-        self.next_state('drop_scale')
-
-    @timed_state(duration=0.5)
-    def drop_scale(self):
-        # TODO: Complete once manipulator configuration known
-        pass
+        if self.switch():
+            self.crane.release()
