55bcc57d779870684f798e068c1a3d24a243db1e
ErikBoesen <me@erikboesen.com>
2018-02-06 16:13:18 -0500

    Increase threshold for compressor deactivation

--- robot/robot.py
+++ robot/robot.py
@@ -145,7 +145,7 @@ class Robot(magicbot.MagicRobot):
         # Read from joysticks and move drivetrain accordingly
         self.drive.move(-self.joystick_left.getY(), self.joystick_right.getX())
 
-        if abs(self.joystick_right.getX()) > 0.25:
+        if abs(self.joystick_right.getX()) > 0.5:
             self.compressor.stop()
         else:
             self.compressor.start()
