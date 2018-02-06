90771b74b675a3280ee8084cee31a7a81060a5d3
ErikBoesen <me@erikboesen.com>
2018-02-05 22:29:15 -0500

    Clean up switch side Modular mode

--- robot/autonomous/modular.py
+++ robot/autonomous/modular.py
@@ -41,27 +41,37 @@ class Modular(AutonomousStateMachine):
         Decide how to begin the autonomous.
         """
         self.crane.grip()
-        # If we are only advancing to cross the auto line,
-        # make the charge sequence longer. Only charge if on the left or right,
-        # so that the robot will not crash into the switch.
-        if self.switch or self.scale:
-            self.next_state('switch_initial')
+        if self.switch:
+            if self.position == 'middle':
+                self.next_state('switch_middle_start')
+            else:
+                self.next_state('switch_side_start')
 
-    @timed_state(duration=1.7, next_state='switch_rotate_side')
-    def switch_side_initial(self):
+    ########
+    # SWITCH
+    # SIDE
+    ########
+    @state
+    def switch_side_start(self):
+        """
+        Initialize switch side autonomous portion.
+        """
+        self.next_state('switch_side_advance')
+
+    @timed_state(duration=1.7, next_state='switch_side_rotate')
+    def switch_side_advance(self):
         """
         Give the robot some distance from the starting point.
         """
         self.crane.move(0.4)
         self.drive.move(0.7, 0)
 
-    @timed_state(duration=0.6, next_state='switch_drop_side')
-    def switch_rotate_side(self):
+    @timed_state(duration=0.6, next_state='switch_side_drop')
+    def switch_side_rotate(self):
         """
         Rotate robot to face the switch.
         """
         self.crane.move(0.4)
-        # Numbers are approximate
         if self.position == 'left':
             self.drive.move(0.2, 1)
         elif self.position == 'right':
@@ -71,18 +81,13 @@ class Modular(AutonomousStateMachine):
                 self.drive.move(0.2, -1)
             elif self.plates[0] == 'R':
                 self.drive.move(0.2, 1)
-        # The robot will now have the opportunity to pursue different paths
-        # It can either try to score on the switch, the switch and the scale,
-        # just the scale, or neither, depending on what is selected in the UI
-        if self.switch:
-            # TODO: Make this do things
-            # self.next_state('switch_curve')
-            pass
 
     @timed_state(duration=0.5)
-    def switch_drop_side(self):
+    def switch_side_drop(self):
         """
         Drop preloaded cube in switch.
         """
-        if self.switch():
+        if self.scale:
+            self.next_state('scale_advance')
+        elif self.switch:
             self.crane.release()
