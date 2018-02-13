906e099a283f32768597bedd1015187384ea28b6
ErikBoesen <me@erikboesen.com>
2018-02-13 08:29:34 -0500

    Restructure camera management without object

--- robot/camera/camera.py
+++ robot/camera/camera.py
@@ -3,27 +3,22 @@
 import cscore
 
 
-class Sight:
-    def __init__(self):
-        self.main_camera = cscore.UsbCamera('Main Camera', 0)
-        self.main_camera.setVideoMode(cscore.VideoMode.PixelFormat.kMJPEG, 160, 120, 30)
-        self.main_camera.setExposureAuto()
-        self.main_camera.getProperty('backlight_compensation').set(5)
-
-        self.main_server = cscore.MjpegServer('httpserver', 1181)
-        self.main_server.setSource(self.main_camera)
-
-        self.intake_camera = cscore.UsbCamera('Intake Camera', 1)
-        self.intake_camera.setVideoMode(cscore.VideoMode.PixelFormat.kMJPEG, 160, 120, 30)
-        self.intake_camera.setExposureAuto()
-        self.intake_camera.getProperty('backlight_compensation').set(5)
-
-        self.intake_server = cscore.MjpegServer('httpserver', 1182)
-        self.intake_server.setSource(self.intake_camera)
-
-
 def main():
-    Sight().process()
+    main_camera = cscore.UsbCamera('Main Camera', 0)
+    main_camera.setVideoMode(cscore.VideoMode.PixelFormat.kMJPEG, 160, 120, 30)
+    main_camera.setExposureAuto()
+    main_camera.getProperty('backlight_compensation').set(5)
+
+    main_server = cscore.MjpegServer('httpserver', 1181)
+    main_server.setSource(main_camera)
+
+    intake_camera = cscore.UsbCamera('Intake Camera', 1)
+    intake_camera.setVideoMode(cscore.VideoMode.PixelFormat.kMJPEG, 160, 120, 30)
+    intake_camera.setExposureAuto()
+    intake_camera.getProperty('backlight_compensation').set(5)
+
+    intake_server = cscore.MjpegServer('httpserver', 1182)
+    intake_server.setSource(intake_camera)
 
 
 if __name__ == '__main__':
