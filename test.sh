9fce293214bac5b86c53966ddb21691a8c9801b2
ErikBoesen <me@erikboesen.com>
2018-02-12 20:45:55 -0500

    Run pycodestyle before pyfrc tests for time saving

--- test.sh
+++ test.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
-python3 robot/robot.py test
 # E501 line too long
 # E701 multiple statements on one line
 # Currently, E701 takes issue with some type annotations. (PyCQA/pycodestyle#682)
 pycodestyle . --ignore=E501,E701
+python3 robot/robot.py test
