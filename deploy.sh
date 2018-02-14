90833e789a8dff11be5cad00184fe16181f6f28a
ErikBoesen <me@erikboesen.com>
2018-02-14 15:20:35 -0500

    In deploy.sh, fetch network PSKs from environment variables

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@
 start_network=$(networksetup -getairportnetwork en0 | cut -d ' ' -f 4)
 robot_network=1418
 
-networksetup -setairportnetwork en0 $robot_network
+networksetup -setairportnetwork en0 $robot_network $DEPLOY_ROBOT_NETWORK_PSK
 python3 robot/robot.py deploy
-networksetup -setairportnetwork en0 $start_network
+networksetup -setairportnetwork en0 $start_network $DEPLOY_START_NETWORK_PSK
