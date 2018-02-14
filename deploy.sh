f620e1e85ec701404510e07f397b28a4ff6663c6
ErikBoesen <me@erikboesen.com>
2018-02-14 15:57:20 -0500

    In deploy.sh, log pending connections to networks

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,8 @@
 start_network=$(networksetup -getairportnetwork en0 | cut -d ' ' -f 4)
 robot_network=1418
 
+echo "Connecting to $robot_network..."
 networksetup -setairportnetwork en0 $robot_network $DEPLOY_ROBOT_NETWORK_PSK
 python3 robot/robot.py deploy
+echo "Reconnecting to $start_network..."
 networksetup -setairportnetwork en0 $start_network $DEPLOY_START_NETWORK_PSK
