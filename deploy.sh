7382fd3a949a60b1b90613a3168c764de35781de
ErikBoesen <me@erikboesen.com>
2018-02-14 16:16:10 -0500

    In deploy.sh, decrease repetition

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 
+function psk_hash {
+    printf $(shasum <<< "$1" | cut -c -8)
+}
+
 start_network=$(networksetup -getairportnetwork en0 | cut -d ' ' -f 4)
 robot_network=1418
 
-echo "Connecting to $robot_network, with PSK hash beginning $(shasum <<< "$DEPLOY_ROBOT_NETWORK_PSK" | cut -c -8)..."
+echo "Connecting to $robot_network, with PSK hash beginning $(psk_hash $DEPLOY_ROBOT_NETWORK_PSK)..."
 networksetup -setairportnetwork en0 $robot_network $DEPLOY_ROBOT_NETWORK_PSK
 python3 robot/robot.py deploy
-echo "Reconnecting to $start_network, with PSK hash beginning $(shasum <<< "$DEPLOY_START_NETWORK_PSK" | cut -c -8)..."
+echo "Reconnecting to $start_network, with PSK hash beginning $(psk_hash $DEPLOY_START_NETWORK_PSK)..."
 networksetup -setairportnetwork en0 $start_network $DEPLOY_START_NETWORK_PSK
