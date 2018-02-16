0868e05911dd5315dd4beffedb4a4979326d6019
ErikBoesen <me@erikboesen.com>
2018-02-16 12:35:42 -0500

    Remove unnecessary comments from Replay autonomous

--- robot/autonomous/replay.py
+++ robot/autonomous/replay.py
@@ -30,21 +30,16 @@ class Replay(AutonomousStateMachine):
 
         TODO: No real reason for this to be stateful.
         """
-        # TODO: Rather than manually controlling components, find a way to
-        # run teleopPeriodic with the recorded input.
+        # TODO: Rather than manually controlling components, run teleopPeriodic with recorded input.
         self.frame_number += 1
         fr = self.recording[self.frame_number]
 
-        # TODO: Destroy in fire
         self.drive.move(-fr['joysticks'][0]['axes'][1], fr['joysticks'][1]['axes'][0])
 
-        # TODO: Please never do a git blame
         if fr['joysticks'][2]['buttons'][1] and not self.recording[self.frame_number - 1]['joysticks'][2]['buttons'][1]:
             self.crane.actuate_claw()
 
-        # TODO: There is no elegance here, only sleep deprivation and regret
         if fr['joysticks'][2]['buttons'][2] and not self.recording[self.frame_number - 1]['joysticks'][2]['buttons'][2]:
             self.crane.actuate_forearm()
 
         self.crane.move(-fr['joysticks'][2]['axes'][1])
-        # TODO: H̵̭͕͖̠̳̞͚͞͞É̫̥̺̮̮̻̮͙̩͖͖̭́͟͜͡͠͡ͅC̸̶̨̱̜̘̦̣̯̲̬͎̖̲̟̤̰̤̱̰͟Ǫ̷͍̱̳̬M̶͙̪̮̣͖̰͍͍̮͓̣̜̝̯̼̲̜̭͝E̸̵̯͙̤̯̩̘̱̙̤̦͎̱͍͜͢S̶̢̳̣̤̟̖͓̝͟͢͠ͅͅ
