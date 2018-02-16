7b508805edd5566f9678f37d8616787b2600d476
ErikBoesen <me@erikboesen.com>
2018-02-15 22:02:49 -0500

    Add functional, if unpleasant, control replay logic

--- robot/autonomous/replay.py
+++ robot/autonomous/replay.py
@@ -11,6 +11,7 @@ class Replay(AutonomousStateMachine):
     DEFAULT = False
 
     recording_name = tunable('')
+    frame_number = 0
 
     @state(first=True)
     def start(self):
@@ -25,4 +26,21 @@ class Replay(AutonomousStateMachine):
 
         TODO: No real reason for this to be stateful.
         """
-        pass
+        # TODO: Rather than manually controlling components, find a way to
+        # run teleopPeriodic with the recorded input.
+        self.frame_number += 1
+        fr = self.recording[self.frame_number]
+
+        # TODO: Destroy in fire
+        self.drive.move(-fr['joysticks'][0]['axes'][1], fr['joysticks'][1]['axes'][0])
+
+        # TODO: Please never do a git blame
+        if fr['joysticks'][2]['buttons'][1] and not self.recording[self.frame_number - 1]['joysticks'][2]['buttons'][1]:
+            self.crane.actuate_claw()
+
+        # TODO: There is no elegance here, only sleep deprivation and regret
+        if fr['joysticks'][2]['buttons'][2] and not self.recording[self.frame_number - 1]['joysticks'][2]['buttons'][2]:
+            self.crane.actuate_forearm()
+
+        self.crane.move(-fr['joysticks'][2]['axes'][1])
+        # TODO: H̵̭͕͖̠̳̞͚͞͞É̫̥̺̮̮̻̮͙̩͖͖̭́͟͜͡͠͡ͅC̸̶̨̱̜̘̦̣̯̲̬͎̖̲̟̤̰̤̱̰͟Ǫ̷͍̱̳̬M̶͙̪̮̣͖̰͍͍̮͓̣̜̝̯̼̲̜̭͝E̸̵̯͙̤̯̩̘̱̙̤̦͎̱͍͜͢S̶̢̳̣̤̟̖͓̝͟͢͠ͅͅ
