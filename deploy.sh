6c64b6d664b2edb5aecfaa165da623e5b9a8158c
ErikBoesen <me@erikboesen.com>
2018-02-16 20:15:39 -0500

    Account for networksetup not returning error codes

--- deploy.sh
+++ deploy.sh
@@ -19,8 +19,12 @@ fi
 start_network=$(networksetup -getairportnetwork en0 | cut -d ' ' -f 4)
 robot_network=1418
 
-echo "Connecting to $robot_network..."
-networksetup -setairportnetwork en0 $robot_network $DEPLOY_ROBOT_NETWORK_PSK
+printf "Connecting to $robot_network... "
+if ! [ "$(networksetup -setairportnetwork en0 $robot_network $DEPLOY_ROBOT_NETWORK_PSK)" = "" ]; then
+    exit 1
+else echo "👍"; fi
 python3 robot/robot.py deploy
-echo "Reconnecting to $start_network..."
-networksetup -setairportnetwork en0 $start_network $DEPLOY_START_NETWORK_PSK
+printf "Reconnecting to $start_network... "
+if ! [ "$(networksetup -setairportnetwork en0 $start_network $DEPLOY_START_NETWORK_PSK)" = "" ]; then
+    exit 1
+else echo "👍"; fi
