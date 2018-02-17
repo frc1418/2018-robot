7ccaef5f187e014fd94e3d5eba4b5c93ab22b042
ErikBoesen <me@erikboesen.com>
2018-02-16 21:41:41 -0500

    While deploying, don't change networks if already on robot network

--- deploy.sh
+++ deploy.sh
@@ -15,30 +15,25 @@ RESET="\e[0m"
 function task { printf "${CYAN}$1... ${RESET}"; }
 function succ { printf "${GREEN}$1${RESET}\n"; }
 function warn { printf "${YELLOW}Warning: $1${RESET}\n" >&2; }
-function err  { printf "${RED}$1${RESET}\n" >&2; exit 1; }
+function fail { printf "${RED}failed.${RESET}\n" >&2; exit 1; }
 
 if ! [ "$(git status --porcelain)" = "" ]; then
     warn "You have uncommitted changes!"
     git status --short
 fi
 
-if [ "$DEPLOY_START_NETWORK_PSK" = "" ]; then
-    warn "\$DEPLOY_START_NETWORK_PSK not set."
-fi
-if [ "$DEPLOY_ROBOT_NETWORK_PSK" = "" ]; then
-    warn "\$DEPLOY_ROBOT_NETWORK_PSK not set."
-fi
+if [ "$DEPLOY_START_NETWORK_PSK" = "" ]; then warn "\$DEPLOY_START_NETWORK_PSK not set."; fi
+if [ "$DEPLOY_ROBOT_NETWORK_PSK" = "" ]; then warn "\$DEPLOY_ROBOT_NETWORK_PSK not set."; fi
 
 start_network=$(networksetup -getairportnetwork en0 | cut -d ' ' -f 4)
 robot_network=1418
 
 function connect {
+    if [ $start_network = $robot_network ]; then return; fi
     task "Connecting to $1"
     # Even when networksetup fails, exit status will bizarrely be 0.
     # Thus, consider no output success.
-    if ! [ "$(networksetup -setairportnetwork en0 $1 $2)" = "" ]; then
-        err "failed."
-    else succ "👍"; fi
+    if ! [ "$(networksetup -setairportnetwork en0 $1 $2)" = "" ]; then fail; else succ; fi
 }
 
 connect $robot_network $DEPLOY_ROBOT_NETWORK_PSK
